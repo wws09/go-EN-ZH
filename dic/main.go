b7e7b3da866f3d821a97d0b65f713173ac0d7ff3
wws09 <13715079446@163.com>
2023-08-14 18:24:34 +0800

    response解析，打印

--- dic/main.go
+++ dic/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -10,6 +11,198 @@ import (
 )
 
 var Data map[string]string = make(map[string]string)
+
+type DictResponse struct {
+	TransResult struct {
+		Data []struct {
+			Dst string `json:"dst"`
+			Src string `json:"src"`
+		} `json:"data"`
+		From     string `json:"from"`
+		To       string `json:"to"`
+		Status   int    `json:"status"`
+		Type     int    `json:"type"`
+		Phonetic []struct {
+			SrcStr string `json:"src_str"`
+			TrgStr string `json:"trg_str"`
+		} `json:"phonetic"`
+	} `json:"trans_result"`
+	DictResult struct {
+		Edict struct {
+			Item []struct {
+				TrGroup []struct {
+					Tr          []string `json:"tr"`
+					Example     []string `json:"example"`
+					SimilarWord []string `json:"similar_word"`
+				} `json:"tr_group"`
+				Pos string `json:"pos"`
+			} `json:"item"`
+			Word string `json:"word"`
+		} `json:"edict"`
+		From        string `json:"from"`
+		SimpleMeans struct {
+			WordName  string   `json:"word_name"`
+			From      string   `json:"from"`
+			WordMeans []string `json:"word_means"`
+			Tags      struct {
+				Core  []string `json:"core"`
+				Other []string `json:"other"`
+			} `json:"tags"`
+			Exchange struct {
+				WordPl []string `json:"word_pl"`
+			} `json:"exchange"`
+			Symbols []struct {
+				PhEn  string `json:"ph_en"`
+				PhAm  string `json:"ph_am"`
+				Parts []struct {
+					Part  string   `json:"part"`
+					Means []string `json:"means"`
+				} `json:"parts"`
+				PhOther string `json:"ph_other"`
+			} `json:"symbols"`
+		} `json:"simple_means"`
+		Common struct {
+			Text string `json:"text"`
+		} `json:"common"`
+		Collins struct {
+			Entry []struct {
+				Type    string `json:"type"`
+				EntryID string `json:"entry_id"`
+				Value   []struct {
+					MeanType []struct {
+						InfoType string `json:"info_type"`
+						InfoID   string `json:"info_id"`
+						Example  []struct {
+							ExampleID string `json:"example_id"`
+							TtsSize   string `json:"tts_size"`
+							Tran      string `json:"tran"`
+							Ex        string `json:"ex"`
+							TtsMp3    string `json:"tts_mp3"`
+						} `json:"example,omitempty"`
+						Posc []struct {
+							Tran    string `json:"tran"`
+							PoscID  string `json:"posc_id"`
+							Example []struct {
+								ExampleID string `json:"example_id"`
+								Tran      string `json:"tran"`
+								Ex        string `json:"ex"`
+								TtsMp3    string `json:"tts_mp3"`
+							} `json:"example"`
+							Def string `json:"def"`
+						} `json:"posc,omitempty"`
+					} `json:"mean_type"`
+					Gramarinfo []struct {
+						Tran  string `json:"tran"`
+						Type  string `json:"type"`
+						Label string `json:"label"`
+					} `json:"gramarinfo"`
+					Tran   string `json:"tran"`
+					Def    string `json:"def"`
+					MeanID string `json:"mean_id"`
+					Posp   []struct {
+						Label string `json:"label"`
+					} `json:"posp"`
+				} `json:"value"`
+			} `json:"entry"`
+			WordName      string `json:"word_name"`
+			WordID        string `json:"word_id"`
+			WordEmphasize string `json:"word_emphasize"`
+			Frequence     string `json:"frequence"`
+		} `json:"collins"`
+		Lang   string `json:"lang"`
+		Oxford struct {
+			Entry []struct {
+				Tag  string `json:"tag"`
+				Name string `json:"name"`
+				Data []struct {
+					Tag  string `json:"tag"`
+					Data []struct {
+						Tag  string `json:"tag"`
+						Data []struct {
+							Tag  string `json:"tag"`
+							Data []struct {
+								Tag  string `json:"tag"`
+								Data []struct {
+									Tag    string `json:"tag"`
+									EnText string `json:"enText,omitempty"`
+									ChText string `json:"chText,omitempty"`
+									G      string `json:"g,omitempty"`
+									Data   []struct {
+										Text      string `json:"text"`
+										HoverText string `json:"hoverText"`
+									} `json:"data,omitempty"`
+								} `json:"data"`
+							} `json:"data"`
+						} `json:"data,omitempty"`
+						P     string `json:"p,omitempty"`
+						PText string `json:"p_text,omitempty"`
+						N     string `json:"n,omitempty"`
+						Xt    string `json:"xt,omitempty"`
+					} `json:"data"`
+				} `json:"data"`
+			} `json:"entry"`
+			Unbox []struct {
+				Tag  string `json:"tag"`
+				Type string `json:"type"`
+				Name string `json:"name"`
+				Data []struct {
+					Tag     string `json:"tag"`
+					Text    string `json:"text,omitempty"`
+					Words   string `json:"words,omitempty"`
+					Outdent string `json:"outdent,omitempty"`
+					Data    []struct {
+						Tag    string `json:"tag"`
+						EnText string `json:"enText"`
+						ChText string `json:"chText"`
+					} `json:"data,omitempty"`
+				} `json:"data"`
+			} `json:"unbox"`
+		} `json:"oxford"`
+		BaiduPhrase []struct {
+			Tit   []string `json:"tit"`
+			Trans []string `json:"trans"`
+		} `json:"baidu_phrase"`
+		Sanyms []struct {
+			Tit  string `json:"tit"`
+			Type string `json:"type"`
+			Data []struct {
+				P string   `json:"p"`
+				D []string `json:"d"`
+			} `json:"data"`
+		} `json:"sanyms"`
+		QueryExplainVideo struct {
+			ID           int    `json:"id"`
+			UserID       string `json:"user_id"`
+			UserName     string `json:"user_name"`
+			UserPic      string `json:"user_pic"`
+			Query        string `json:"query"`
+			Direction    string `json:"direction"`
+			Type         string `json:"type"`
+			Tag          string `json:"tag"`
+			Detail       string `json:"detail"`
+			Status       string `json:"status"`
+			SearchType   string `json:"search_type"`
+			FeedURL      string `json:"feed_url"`
+			Likes        string `json:"likes"`
+			Plays        string `json:"plays"`
+			CreatedAt    string `json:"created_at"`
+			UpdatedAt    string `json:"updated_at"`
+			DuplicateID  string `json:"duplicate_id"`
+			RejectReason string `json:"reject_reason"`
+			CoverURL     string `json:"coverUrl"`
+			VideoURL     string `json:"videoUrl"`
+			ThumbURL     string `json:"thumbUrl"`
+			VideoTime    string `json:"videoTime"`
+			VideoType    string `json:"videoType"`
+		} `json:"queryExplainVideo"`
+	} `json:"dict_result"`
+	LijuResult struct {
+		Double string   `json:"double"`
+		Tag    []string `json:"tag"`
+		Single string   `json:"single"`
+	} `json:"liju_result"`
+	Logid int `json:"logid"`
+}
 
 func query(word string, sign string) {
 	client := &http.Client{}
@@ -62,7 +255,20 @@ func query(word string, sign string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+	// fmt.Printf("%s\n", bodyText)
+
+	var dictResponse DictResponse
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%#v\n", dictResponse)
+
+	fmt.Println(word, "UK/", dictResponse.DictResult.SimpleMeans.Symbols[0].PhEn, "US/", dictResponse.DictResult.SimpleMeans.Symbols[0].PhAm)
+	for _, item := range dictResponse.DictResult.SimpleMeans.Symbols[0].Parts {
+		fmt.Println(item)
+	}
+
 }
 
 func main() {
